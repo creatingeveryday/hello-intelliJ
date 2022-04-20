be3a94b8bc1d5b6638d37125bae4a6760cc1b143
JongKuk Seo <jongkuk.dev@gmail.com>
2022-04-20 12:35:48 +0900

    init

---
+++ src/test/java/hello/hellointellij/HelloIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellointellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloIntellijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
